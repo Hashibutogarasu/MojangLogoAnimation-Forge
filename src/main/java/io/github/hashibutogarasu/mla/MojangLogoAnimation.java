7f70cbd16959e270bff65dc25c7087e71431a0dd
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-05-12 00:14:27 +0900

    Deleted AutoConfig Screen

--- src/main/java/io/github/hashibutogarasu/mla/MojangLogoAnimation.java
+++ src/main/java/io/github/hashibutogarasu/mla/MojangLogoAnimation.java
@@ -5,14 +5,18 @@ import io.github.hashibutogarasu.mla.config.ModConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import static net.minecraftforge.fml.config.ModConfig.Type.COMMON;
 
 @Mod(MojangLogoAnimation.MODID)
 public class MojangLogoAnimation {
@@ -22,7 +26,7 @@ public class MojangLogoAnimation {
     public MojangLogoAnimation() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
-//        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        ModLoadingContext.get().registerConfig(COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
