7acb028e71de0361ee54317e72e7e7f2ff180512
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-05-13 12:03:19 +0900

    Deleted EventBusSubscriber

--- src/main/java/io/github/hashibutogarasu/mla/config/ModConfig.java
+++ src/main/java/io/github/hashibutogarasu/mla/config/ModConfig.java
@@ -1,18 +1,10 @@
 package io.github.hashibutogarasu.mla.config;
 
-import io.github.hashibutogarasu.mla.MojangLogoAnimation;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.common.Mod;
 
 @Config(name = "mla")
-@Mod.EventBusSubscriber(modid = MojangLogoAnimation.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModConfig implements ConfigData {
-    public ModConfig(ForgeConfigSpec.Builder builder){
-
-    }
-
-
     public Mode mode = Mode.MOJANG;
 }
